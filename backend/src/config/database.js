950c78ad9244ed66986ebbee8c653c308dbd59bf
kaoutar <157612445+Kaoutarkham@users.noreply.github.com>
2026-01-13 12:00:07 +0100

    instalation sequelize

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,1 +1,12 @@
+const { Sequelize } = require("sequelize");
+const config = require("../../config/config.js").development;
 
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+module.exports = sequelize;
