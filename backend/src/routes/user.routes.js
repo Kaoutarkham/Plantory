3dcd19427cd49a889b1d5f8488cf16c2ff3a6d3c
kaoutar <157612445+Kaoutarkham@users.noreply.github.com>
2026-01-25 16:56:25 +0100

    plants and users routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,17 +4,12 @@ const userController = require("../controllers/user.controller");
 const upload = require("../middlewares/multer");
 const auth = require("../middlewares/auth");
 
-// Vérifie que userController.register existe bien !
 router.post(
   "/register",
-  upload.single("profileImage"),
+  upload.single("profilePicture"),
   userController.register,
 );
-
 router.post("/login", userController.login);
-
-// Vérifie que le nom 'getProfile' est identique dans le contrôleur
-router.get("/profile/:id", userController.getProfile);
-router.get("/profile", auth, userController.getProfile);
+router.get("/profile/:id", auth, userController.getProfile);
 
 module.exports = router;
